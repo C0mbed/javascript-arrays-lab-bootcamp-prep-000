e0ff73823fd812be16559b3a8fef582f2e252a23
Matt Farncombe <matt@farncombe.co>
2020-04-16 20:14:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
-function destructivelyAppendKitten(kittens, name) {
+function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
 }
 
 function destructivelyPrependKitten(kittens, name) {
   kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(kittens) {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(kittens) {
+  kittens.shift();
   return kittens;
 }
