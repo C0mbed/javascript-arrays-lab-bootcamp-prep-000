436d1965f1d805dc8a6a5c2681206958823dce33
Matt Farncombe <matt@farncombe.co>
2020-04-16 20:12:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function destructivelyAppendKitten(kittens, name) {
   kittens.push(name);
   return kittens;
 }
+
+function destructivelyPrependKitten(kittens, name) {
+  kittens.unshift(name);
+  return kittens;
+}
